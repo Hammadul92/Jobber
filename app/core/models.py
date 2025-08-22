fab8cc9b4e8b00c1dc1e3546c1f6d1c90e8cd565
hammad <hammad17624@gmail.com>
2025-08-22 05:37:49 -0600

    added react typescript frontend

--- app/core/models.py
+++ app/core/models.py
@@ -2,11 +2,91 @@
 Database models.
 """
 from django.db import models
+from django.conf import settings
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
 )
+
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+COUNTRY_CHOICES = [
+    ("CA", "Canada"),
+    ("US", "United States"),
+]
+
+PROVINCES_CA = [
+    ("AB", "Alberta"),
+    ("BC", "British Columbia"),
+    ("MB", "Manitoba"),
+    ("NB", "New Brunswick"),
+    ("NL", "Newfoundland and Labrador"),
+    ("NS", "Nova Scotia"),
+    ("NT", "Northwest Territories"),
+    ("NU", "Nunavut"),
+    ("ON", "Ontario"),
+    ("PE", "Prince Edward Island"),
+    ("QC", "Quebec"),
+    ("SK", "Saskatchewan"),
+    ("YT", "Yukon"),
+]
+
+STATES_US = [
+    ("AL", "Alabama"),
+    ("AK", "Alaska"),
+    ("AZ", "Arizona"),
+    ("AR", "Arkansas"),
+    ("CA", "California"),
+    ("CO", "Colorado"),
+    ("CT", "Connecticut"),
+    ("DE", "Delaware"),
+    ("FL", "Florida"),
+    ("GA", "Georgia"),
+    ("HI", "Hawaii"),
+    ("ID", "Idaho"),
+    ("IL", "Illinois"),
+    ("IN", "Indiana"),
+    ("IA", "Iowa"),
+    ("KS", "Kansas"),
+    ("KY", "Kentucky"),
+    ("LA", "Louisiana"),
+    ("ME", "Maine"),
+    ("MD", "Maryland"),
+    ("MA", "Massachusetts"),
+    ("MI", "Michigan"),
+    ("MN", "Minnesota"),
+    ("MS", "Mississippi"),
+    ("MO", "Missouri"),
+    ("MT", "Montana"),
+    ("NE", "Nebraska"),
+    ("NV", "Nevada"),
+    ("NH", "New Hampshire"),
+    ("NJ", "New Jersey"),
+    ("NM", "New Mexico"),
+    ("NY", "New York"),
+    ("NC", "North Carolina"),
+    ("ND", "North Dakota"),
+    ("OH", "Ohio"),
+    ("OK", "Oklahoma"),
+    ("OR", "Oregon"),
+    ("PA", "Pennsylvania"),
+    ("RI", "Rhode Island"),
+    ("SC", "South Carolina"),
+    ("SD", "South Dakota"),
+    ("TN", "Tennessee"),
+    ("TX", "Texas"),
+    ("UT", "Utah"),
+    ("VT", "Vermont"),
+    ("VA", "Virginia"),
+    ("WA", "Washington"),
+    ("WV", "West Virginia"),
+    ("WI", "Wisconsin"),
+    ("WY", "Wyoming"),
+]
 
 
 class UserManager(BaseUserManager):
@@ -42,3 +122,27 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Business(models.Model):
+    """Business entity (like a company in Jobber)."""
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="owned_businesses",
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255, blank=False, null=False)
+    # Address Fields
+    street_address = models.CharField(max_length=255, blank=False, null=False)
+    city = models.CharField(max_length=100, blank=False, null=False)
+    country = models.CharField(max_length=2, choices=COUNTRY_CHOICES, default="CA")
+    province_state = models.CharField(max_length=2, blank=False, null=False)
+    postal_code = models.CharField(max_length=10, blank=False, null=False)
+    phone = PhoneNumberField(blank=False, null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
