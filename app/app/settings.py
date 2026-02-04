cd5b77fd59018caaf1826ac8097a231c64090db4
hammad <hammad17624@gmail.com>
2026-02-03 19:15:53 -0700

    Make docker-compose and Django settings production-ready: use env vars for DB and ALLOWED_HOSTS

--- app/app/settings.py
+++ app/app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
