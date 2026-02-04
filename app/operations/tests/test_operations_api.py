6c7c62ed275e59443196285aec8cfabdfde6a25c
hammad <hammad17624@gmail.com>
2026-02-03 21:08:12 -0700

    fixed tests

--- app/operations/tests/test_operations_api.py
+++ app/operations/tests/test_operations_api.py
@@ -1,6 +1,7 @@
 """
 Test for business APIs
 """
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -13,22 +14,22 @@ from core.models import Business
 from operations.serializers import BusinessSerializer
 
 
-BUSINESSES_URL = reverse('operations:business-list')
+BUSINESSES_URL = reverse("operations:business-list")
 
 
 def create_business(owner, **params):
-    """ Create and return a sample business """
+    """Create and return a sample business."""
     defaults = {
-        'name': 'Test Business',
-        'phone': '1234567890',
-        'email': 'test_business@example.com',
-        'business_description': 'Test business description',
-        'street_address': '123 test street',
-        'city': 'Calgary',
-        'country': 'CA',
-        'province_state': 'AB',
-        'business_number': '123456789',
-        'tax_rate': 5
+        "name": "Test Business",
+        "phone": "1234567890",
+        "email": "test_business@example.com",
+        "business_description": "Test business description",
+        "street_address": "123 test street",
+        "city": "Calgary",
+        "country": "CA",
+        "province_state": "AB",
+        "business_number": "123456789",
+        "tax_rate": 5,
     }
 
     defaults.update(**params)
@@ -56,8 +57,8 @@ class PrivateBusinessApiTests(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = get_user_model().objects.create_user(
-            'test@example.com',
-            'test123',
+            "test@example.com",
+            "test123",
         )
         self.client.force_authenticate(self.user)
 
@@ -66,7 +67,7 @@ class PrivateBusinessApiTests(TestCase):
         create_business(owner=self.user)
 
         res = self.client.get(BUSINESSES_URL)
-        businesses = Business.objects.all().order_by('-id')
+        businesses = Business.objects.all().order_by("-id")
         serializer = BusinessSerializer(businesses, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
@@ -74,14 +75,14 @@ class PrivateBusinessApiTests(TestCase):
     def test_business_list_limited_to_user(self):
         """Test list of businesses is limited to authenticated user."""
         other_user = get_user_model().objects.create_user(
-            'other@example.com',
-            'test123',
+            "other@example.com",
+            "test123",
         )
         create_business(owner=other_user)
         create_business(owner=self.user)
 
         res = self.client.get(BUSINESSES_URL)
-        businesses = Business.objects.filter(owner=self.user).order_by('-id')
+        businesses = Business.objects.filter(owner=self.user).order_by("-id")
         serializer = BusinessSerializer(businesses, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
