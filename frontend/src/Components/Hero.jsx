cb2d4f612df35e6f8d393f0480abe3351edb74bb
hammad <hammad17624@gmail.com>
2026-02-12 09:20:29 -0700

    another check

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -71,7 +71,7 @@ function Hero() {
                 {/* Main Heading */}
                 <h1 className='font-heading text-[1.2rem] md:text-4xl lg:text-[3.2rem] text-center leading-snug md:max-w-7xl'>
                     OPERATE YOUR BUSINESS WITH <span className='text-accent'>ASSURANCE</span>
-                    AND <span className='text-accent'>CLARITY</span>
+                    AND <span className='text-accent'>CLARITY</span>.
                 </h1>
                 {/* Subheading */}
                 <p className='text-center lg:px-28 text-2xl section-intro mt-2'>
