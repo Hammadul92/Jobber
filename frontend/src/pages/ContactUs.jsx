b60ca6ba7b23ba86806b1da7b9fa560bcea9de1d
Ali Ahsan <aliahsann15@gmail.com>
2026-02-17 03:13:37 +0500

    fix import paths for FAQSection and CTASection in ContactUs component

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ContactHero from '../Components/ContactPage/ContactHero.jsx'
 import QuickActions from '../Components/ContactPage/QuickActions.jsx'
 import ContactForm from '../Components/ContactPage/ContactForm.jsx'
-import FAQSection from '../components/HomePage/FAQSection'
-import CTASection from '../components/CTASection'
+import FAQSection from '../Components/HomePage/FAQSection'
+import CTASection from '../Components/CTASection'
 
 function ContactUs() {
     return (
